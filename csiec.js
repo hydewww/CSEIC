9c366351393921c66a4f73e8d1e9c96c186983ea
hydewww <2208493266@qq.com>
2017-10-30 17:04:25 +0800

    fix replace

--- csiec.js
+++ csiec.js
@@ -1,6 +1,6 @@
 var forms = document.getElementsByClassName("formulation clearfix");
 for (var i=0;i<forms.length;i++)
 {
-    var word = forms[i].getElementsByTagName("audio")[0].src.split('/')[4].split('.')[0].replace("%20", " ");
+    var word = forms[i].getElementsByTagName("audio")[0].src.split('/')[4].split('.')[0].replace(/%20/g, " ");
     forms[i].getElementsByTagName("input")[1].value = word;
 }
